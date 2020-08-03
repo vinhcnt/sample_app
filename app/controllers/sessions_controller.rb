64c86c1a36dd3cbad80378c35ed704e29fdca8d8
Nguyen The Vinh C <nguyen.the.vinhc@sun-asterisk.com>
2020-08-03 15:48:23 +0700

    Chap 9: Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,20 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
+      if params[:session][:remember_me] == Settings.checkbox
+        remember user
+      else
+        forget user
+      end
       redirect_to user
     else
-      flash.now[:danger] = t "invalid_email_password"
+      flash.now[:danger] = t "shared.invalid_email_password"
       render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_013730) do
+ActiveRecord::Schema.define(version: 2020_08_03_061232) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_07_30_013730) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
